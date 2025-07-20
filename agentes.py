3a45a252034bd20accdac0a11658f8a545ba68a2
Luis Gustavo <luis.benevides@aluno.uece.br>
2025-07-20 14:58:54 -0300

    sync

--- agentes.py
+++ agentes.py
@@ -35,32 +35,31 @@ class AgenteBaseadoEmModelo(Agente):
 
     def __init__(self):
         super().__init__()
-        self.mapa = {}
+        self.mapa = {}  
         self.visitados = set()
+        self.posicao_interna = (0,0)
 
     def agir(self):
         percept_atual, vizinhos = self.ambiente.percept(self.posicao)
-        self.mapa[self.posicao] = percept_atual
-        self.visitados.add(self.posicao)
+
+
+        self.atualizar_estado_interno(vizinhos)
+        self.visitados.add(self.posicao_interna)
+        self.mapa[self.posicao_interna] = percept_atual
 
         if percept_atual == "SUJO":
-            if self.ambiente.sofrer_acao(self, "LIMPAR") == "LIMPOU":   
-                self.pontuacao += 1 # Se limpou, ganha ponto
+            if self.ambiente.sofrer_acao(self, "LIMPAR") == "LIMPOU":
+                self.pontuacao += 1  # Se limpou, ganha ponto
+                self.mapa[self.posicao_interna] = "LIMPO"
             return
 
         # Procurar vizinho não visitado
         for direcao, estado in vizinhos.items():
-            dx, dy = {
-                "CIMA": (0, -1),
-                "BAIXO": (0, 1),
-                "ESQUERDA": (-1, 0),
-                "DIREITA": (1, 0)
-            }[direcao]
-            viz = (self.posicao[0] + dx, self.posicao[1] + dy)
+            viz = self.coordenadas_vizinho(direcao)
             if estado == "LIVRE" and viz not in self.visitados:
-                self.ambiente.sofrer_acao(self, direcao)
-                print(direcao)
-                self.pontuacao -= 1
+                if self.ambiente.sofrer_acao(self, direcao)== "MOVEU":
+                    self.posicao_interna = viz  
+                    self.pontuacao -= 1
                 return
 
         # Senão, mover para qualquer vizinho livre
@@ -68,4 +67,25 @@ class AgenteBaseadoEmModelo(Agente):
         if livres:
             direcao = random.choice(livres)
             self.ambiente.sofrer_acao(self, direcao)
+            nova_pos = self.coordenadas_vizinho(direcao)
+            self.posicao_interna = nova_pos
             self.pontuacao -= 1
+
+    def atualizar_estado_interno(self, vizinhos):
+        for direcao, estado in vizinhos.items():
+            viz = self.coordenadas_vizinho(direcao)
+            if estado == "LIVRE":
+                self.mapa[viz] = "LIVRE"
+            elif estado == "OBSTACULO":
+                self.mapa[viz] = "OBSTACULO"
+            else:
+                self.mapa[viz] = "PAREDE"
+
+    def coordenadas_vizinho(self, direcao):
+        dx, dy = {
+                "CIMA": (0, -1),
+                "BAIXO": (0, 1),
+                "ESQUERDA": (-1, 0),
+                "DIREITA": (1, 0)
+            }[direcao]
+        return (self.posicao_interna[0] + dx, self.posicao_interna[1] + dy)
